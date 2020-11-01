19560614bb1345177fe5fca7cabd465426307815
lcw1301 <chenweilam@gmail.com>
2020-11-01 19:55:04 +1300

    update

--- config.py
+++ config.py
@@ -8,3 +8,7 @@ class Config:
     FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
     FLASK_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REPOSITORY = environ.get('REPOSITORY')
