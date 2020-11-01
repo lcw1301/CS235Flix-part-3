fac8ab19c4602d5402ad76ea38b709f2290c45d3
lcw1301 <chenweilam@gmail.com>
2020-11-01 19:55:32 +1300

    compatibility with database repository

--- movie_app/__init__.py
+++ movie_app/__init__.py
@@ -1,5 +1,12 @@
 import os
 from flask import Flask
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.pool import NullPool
+
+from movie_app.adapters import database_repository
+from movie_app.adapters.orm import metadata, map_model_to_tables
 import movie_app.adapters.repository as repo
 
 
@@ -12,6 +19,29 @@ def create_app(test_config=None):
     if test_config is not None:
         app.config.from_mapping(test_config)
         data_path = app.config['TEST_DATA_PATH']
+
+    if app.config['REPOSITORY'] == 'database':
+        database_uri = app.config['SQLALCHEMY_DATABASE_URI']
+        database_echo = app.config['SQLALCHEMY_ECHO']
+        database_engine = create_engine(database_uri, connect_args={"check_same_thread": False}, poolclass=NullPool,
+                                        echo=database_echo)
+
+        if app.config['TESTING'] == 'True' or len(database_engine.table_names()) == 0:
+            print("REPOPULATING DATABASE")
+            clear_mappers()
+            metadata.create_all(metadata.sorted_tables)
+            for table in reversed(metadata.sorted_tables):
+                database_engine.execute(table.delete())
+
+            map_model_to_tables()
+
+            database_repository.populate(database_engine, data_path)
+
+        else:
+            map_model_to_tables()
+
+        session_factory = sessionmaker(autocommit=False, autoflush=True, bind=database_engine)
+        repo.repo_instance = database_repository.SQLAlchemyRepository(session_factory)
 
     with app.app_context():
 
