b22b6c9047fc8dfd725b4df6bfb8594c0fcf73ca
lcw1301 <chenweilam@gmail.com>
2020-11-01 19:56:07 +1300

    different mapping method

--- movie_app/adapters/orm.py
+++ movie_app/adapters/orm.py
@@ -1,87 +1,42 @@
 from sqlalchemy import (
-    MetaData, Table, Column, String, Integer, Text, Float, ForeignKey
+    MetaData, Table, Column, String, Integer, Float, Text
 )
 from sqlalchemy.orm import mapper
 
 from movie_app.domain.movie import Movie
-from movie_app.domain.director import Director
-from movie_app.domain.actor import Actor
-from movie_app.domain.genre import Genre
+
 
 metadata = MetaData()
 
 movies = Table(
     'movies', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('rank', Integer),
-    Column('title', String(255)),
-    Column('description', Text),
-    Column('release_year', Integer),
-    Column('runtime_minutes', Float),
-    Column('rating', Float),
-    Column('votes', Integer),
-    Column('revenue', Float),
-    Column('metascore', Float)
-)
-
-directors = Table(
-    'directors', metadata,
-    Column('id', Integer),
-    Column('director_full_name', String(255), unique=True, nullable=False)
-)
-
-genres = Table(
-    'genres', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('genre_name', String(255), unique=True, nullable=False)
-)
-
-movie_genres = Table(
-    'movie_genres', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('movie_id', Integer, ForeignKey('movies.id')),
-    Column('genre_id', Integer, ForeignKey('genres.id'))
-)
-
-actors = Table(
-    'actors', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('actor_full_name', String(255), unique=True, nullable=False)
-)
-
-movie_actors = Table(
-    'movie_actors', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('movie_id', Integer, ForeignKey('movies.id')),
-    Column('actor_id', Integer, ForeignKey('actors.id'))
+    Column('rank', Integer, primary_key=True, autoincrement=True),
+    Column('title', String(255), nullable=True),
+    Column('genre', String(255), nullable=True),
+    Column('description', Text, nullable=True),
+    Column('director', String(255), nullable=True),
+    Column('actors', String(255), nullable=True),
+    Column('release_year', Integer, nullable=True),
+    Column('runtime_minutes', Integer, nullable=True),
+    Column('rating', Float, nullable=True),
+    Column('votes', Integer, nullable=True),
+    Column('revenue', Float, nullable=True),
+    Column('metascore', Integer, nullable=True),
 )
 
 
 def map_model_to_tables():
     mapper(Movie, movies, properties={
-        '__id': movies.c.id,
-        '__rank': movies.c.rank,
-        '__title': movies.c.title,
-        '__description': movies.c.description,
-        '__release_year': movies.c.release_year,
-        '__runtime_minutes': movies.c.runtime_minutes,
-        '__rating': movies.c.rating,
-        '__votes': movies.c.votes,
-        '__revenue:': movies.c.revenue,
-        '__metascore': movies.c.metascore
-    })
-
-    mapper(Director, directors, properties={
-        '__id': directors.c.id,
-        '__director_full_name': directors.c.director_full_name
-    })
-
-    mapper(Actor, actors, properties={
-        '__id': actors.c.id,
-        '__actor_full_name': actors.c.actor_full_name
-    })
-
-    mapper(Genre, genres, properties={
-        '__id': genres.c.id,
-        '__genre_name': genres.c.genre_name
+        '_rank': movies.c.rank,
+        '_title': movies.c.title,
+        '_genre': movies.c.genre,
+        '_description': movies.c.description,
+        '_director': movies.c.director,
+        '_actors': movies.c.actors,
+        '_release_year': movies.c.release_year,
+        '_runtime_minutes': movies.c.runtime_minutes,
+        '_rating': movies.c.rating,
+        '_votes': movies.c.votes,
+        '_revenue:': movies.c.revenue,
+        '_metascore': movies.c.metascore
     })
