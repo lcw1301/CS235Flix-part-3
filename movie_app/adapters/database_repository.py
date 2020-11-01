9f2bd91739d1cd047ba67e01524195a0fbc2f4fb
lcw1301 <chenweilam@gmail.com>
2020-11-01 18:43:17 +1300

    replaced memory repository with database repository that uses sqlalchemy

--- movie_app/adapters/database_repository.py
+++ movie_app/adapters/database_repository.py
@@ -15,8 +15,6 @@ from movie_app.domain.genre import Genre
 from movie_app.domain.movie import Movie
 from movie_app.adapters.repository import AbstractRepository
 
-tags = None
-
 
 class SessionContextManager:
 
@@ -47,3 +45,38 @@ class SessionContextManager:
     def close_current_session(self):
         if not self.__session is None:
             self.__session.close()
+
+
+class SQLAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session_factory):
+        self.__session_cm = SessionContextManager(session_factory)
+
+    def close_session(self):
+        self.__session_cm.close_current_session()
+
+    def reset_session(self):
+        self.__session_cm.reset_session()
+
+    def get_movie(self, id):
+        movie = self.__session_cm.session.query(Movie).filter(Movie.__id == id).first()
+        return movie
+
+
+def load_genres(session, data_path: str):
+    genres = dict()
+    with open(os.path.join(data_path, 'Data1000Movies.csv'), mode='r', encoding='utf-8-sig') as csv_file:
+        movie_file_reader = csv.DictReader(csv_file)
+
+        for row in movie_file_reader:
+            for genre in row['Genre'].split(','):
+                genre = genre.strip()
+                if genre not in genres.keys():
+                    g = Genre(genre)
+                    genres[genre] = g
+        session.commit()
+    return genres
+
+
+def load_actors(session, data_path: str):
+    actors = dict()
